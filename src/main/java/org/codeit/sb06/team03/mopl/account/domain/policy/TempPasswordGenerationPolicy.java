87d00f16fd1a54458dc0a255b3b4121449775969
chiho0270 <96578548+chiho0270@users.noreply.github.com>
2026-02-06 10:32:45 +0900

    feat(account): 비밀번호 초기화 비밀번호 생성 정책 구현

---
+++ src/main/java/org/codeit/sb06/team03/mopl/account/domain/policy/TempPasswordGenerationPolicy.java
@@ -0,0 +1,8 @@
+package org.codeit.sb06.team03.mopl.account.domain.policy;
+
+import org.codeit.sb06.team03.mopl.account.domain.vo.Password;
+
+public interface TempPasswordGenerationPolicy {
+
+    Password generate();
+}
