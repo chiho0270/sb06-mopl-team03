def6a22c54717ef013ce791383112c6e9a2c3dcb
chiho0270 <96578548+chiho0270@users.noreply.github.com>
2026-02-08 01:10:11 +0900

    ref(account): EventListener, Mapper 리팩토링

--- src/main/java/org/codeit/sb06/team03/mopl/email/infra/in/EmailEventListener.java
+++ src/main/java/org/codeit/sb06/team03/mopl/email/infra/in/EmailEventListener.java
@@ -1,9 +1,9 @@
 package org.codeit.sb06.team03.mopl.email.infra.in;
 
 import lombok.RequiredArgsConstructor;
+import org.codeit.sb06.team03.mopl.account.domain.event.AccountEvent.PasswordResetedEvent;
 import org.codeit.sb06.team03.mopl.email.application.in.SendEmailCommand;
 import org.codeit.sb06.team03.mopl.email.application.in.SendEmailUseCase;
-import org.codeit.sb06.team03.mopl.account.domain.event.AccountEvent.PasswordResetedEvent;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
@@ -11,7 +11,7 @@ import org.springframework.transaction.event.TransactionalEventListener;
 
 @RequiredArgsConstructor
 @Component
-public class EmailEventListener{
+public class EmailEventListener {
 
     private final SendEmailUseCase sendEmailUseCase;
     private final PasswordResetMapper passwordResetMapper;
@@ -20,7 +20,6 @@ public class EmailEventListener{
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void handleResetPasswordEvent(PasswordResetedEvent event) {
         SendEmailCommand command = passwordResetMapper.toCommand(event);
-
         sendEmailUseCase.sendEmail(command);
     }
 }
--- src/main/java/org/codeit/sb06/team03/mopl/email/infra/in/PasswordResetMapper.java
+++ src/main/java/org/codeit/sb06/team03/mopl/email/infra/in/PasswordResetMapper.java
@@ -1,11 +1,10 @@
 package org.codeit.sb06.team03.mopl.email.infra.in;
 
-import org.codeit.sb06.team03.mopl.account.application.in.RegisterAccountCommand;
 import org.codeit.sb06.team03.mopl.account.domain.event.AccountEvent;
-import org.codeit.sb06.team03.mopl.account.domain.vo.EmailAddress;
 import org.codeit.sb06.team03.mopl.email.application.in.SendEmailCommand;
-import org.codeit.sb06.team03.mopl.user.infra.in.UserCreateRequest;
 import org.springframework.stereotype.Component;
+
+import java.time.Instant;
 
 @Component
 public class PasswordResetMapper {
@@ -13,13 +12,7 @@ public class PasswordResetMapper {
     public SendEmailCommand toCommand(AccountEvent.PasswordResetedEvent event) {
         final String to = event.getEmailAddress();
         final String rawTempPassword = event.getRawTempPassword();
-        final String expiresAt = event.getExpiresAt();
+        final Instant expiresAt = Instant.parse(event.getExpiresAt());
         return new SendEmailCommand(to, rawTempPassword, expiresAt);
     }
-        public RegisterAccountCommand toCommand(UserCreateRequest request) {
-            final String name = request.name();
-            final EmailAddress emailAddress = new EmailAddress(request.email());
-            final String rawPassword = request.password();
-            return new RegisterAccountCommand(name, emailAddress, rawPassword);
-        }
 }
